5910cd07ea540bb4f4034d558d88e4a3c6008446
Rayen Almeida <139657509+almeidarayen@users.noreply.github.com>
2023-07-16 14:12:49 -0300

    class_03.js

---
+++ class_03.js
@@ -0,0 +1,10 @@
+const age = 8;
+const name = "Rayen";
+
+if ( age >= 18 || name === "Rayen") {
+    console.log('This usuary can enter')
+} else {
+    console.log("No entry")
+}
+
+// = um igual é uma atribuição dois == é um comparativo e === é um comparativo mais preciso
